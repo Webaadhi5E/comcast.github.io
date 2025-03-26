c7518608489b0cbc60709b0bf9a6a07a7fef82f0
aadhi <aadthiya.ah@gmail.com>
2025-03-26 11:39:56 +0530

    added new components and json data enhanced

--- src/components/MultiSelect/multiselect-name.js
+++ src/components/MultiSelect/multiselect-name.js
@@ -1,14 +1,12 @@
 export const mulitiselectNames = [
-    "Oliver Hansen",
-    "Van Henry",
-    "April Tucker",
-    "Ralph Hubbard",
-    "Omar Alexander",
-    "Carlos Abbott",
-    "Miriam Wagner",
-    "Bradley Wilkerson",
-    "Virginia Andrews",
-    "Kelly Snyder",
+  "Oliver Hansen",
+  "Van Henry",
+  "April Tucker",
+  "Ralph Hubbard",
+  "Omar Alexander",
+  "Carlos Abbott",
+  "Miriam Wagner",
+  "Bradley Wilkerson",
+  "Virginia Andrews",
+  "Kelly Snyder",
 ];
-
-export default mulitiselectNames;
--- src/pages/research_areas.jsx
+++ src/pages/research_areas.jsx
@@ -14,6 +14,12 @@ import {
   pages,
 } from "src/data/research_areas.json";
 import { data } from "src/data/research_areas_data.json";
+import Clear from "src/components/Icon/Clear";
+import SearchIcon from "src/components/Icon/Search";
+import ClearBadge from "src/components/Icon/Clear-icon";
+import PlayIcon from "src/components/Icon/Play-icon";
+import PaperIcon from "src/components/Icon/Paper-icon";
+import FileIcon from "src/components/Icon/File-icon";
 
 const ResearchAreas = () => {
   const [searchResults, setSearchResults] = useState(data);
@@ -23,10 +29,10 @@ const ResearchAreas = () => {
   const [showClearAll, setShowClearAll] = useState(false);
   const { title } = useRouter().query;
   const tagShortNames = {
-    cybersecurity: "cy",
-    privacy_engineering: "pr",
-    emerging_technologies: "et",
-    evidence_based_public_policy: "ev",
+    cybersecurity: "cyber_sec",
+    privacy_engineering: "priv_eng",
+    emerging_technologies: "emerging_tech",
+    evidence_based_public_policy: "evi_pub_policy",
   };
 
   function execute_search(value) {
@@ -201,8 +207,9 @@ const ResearchAreas = () => {
                 className={`clear_all ${showClearAll ? "show" : "hide"}`}
                 onClick={clearAll}
               >
-                <p>Clear</p>
-                <svg
+                <div>Clear</div>
+                <Clear />
+                {/* <svg
                   width="16"
                   height="16"
                   fill="currentColor"
@@ -210,7 +217,7 @@ const ResearchAreas = () => {
                   viewBox="0 0 15 15"
                 >
                   <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
-                </svg>
+                </svg> */}
                 {/* <Icon type="clear" color="black" /> */}
               </div>
             </div>
@@ -224,7 +231,8 @@ const ResearchAreas = () => {
                 value={searchTerm}
                 onChange={(event) => execute_search(event.target.value)}
               />
-              <svg
+              <SearchIcon />
+              {/* <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="16"
                 height="16"
@@ -233,7 +241,7 @@ const ResearchAreas = () => {
                 viewBox="0 0 16 16"
               >
                 <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
-              </svg>
+              </svg> */}
             </div>
             <div className="results_count">
               1-{filterResultCount} of {data.length} Results
@@ -282,7 +290,10 @@ const ResearchAreas = () => {
                   selectedYears.map((item, index) => (
                     <div key={index} className="badge">
                       {item.category}
-                      <svg
+                      <ClearBadge
+                        onClick={() => remove_filter(item.category, 1)}
+                      ></ClearBadge>
+                      {/* <svg
                         onClick={() => remove_filter(item.category, 1)}
                         xmlns="http://www.w3.org/2000/svg"
                         width="16"
@@ -292,14 +303,17 @@ const ResearchAreas = () => {
                         viewBox="0 0 16 16"
                       >
                         <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
-                      </svg>
+                      </svg> */}
                     </div>
                   ))}
                 {selectedPublications &&
                   selectedPublications.map((item, index) => (
                     <div key={index} className="badge">
                       {item.category}
-                      <svg
+                      <ClearBadge
+                        onClick={() => remove_filter(item.category, 0)}
+                      ></ClearBadge>
+                      {/* <svg
                         onClick={() => remove_filter(item.category, 0)}
                         xmlns="http://www.w3.org/2000/svg"
                         width="16"
@@ -309,7 +323,7 @@ const ResearchAreas = () => {
                         viewBox="0 0 16 16"
                       >
                         <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
-                      </svg>
+                      </svg> */}
                     </div>
                   ))}
               </div>
@@ -339,42 +353,45 @@ const ResearchAreas = () => {
                           <div className="attachment_button">
                             <div class="icons">
                               {item.icon === "video" && (
-                                <svg
-                                  xmlns="http://www.w3.org/2000/svg"
-                                  width="20"
-                                  height="20"
-                                  fill="#fff"
-                                  class="bi bi-play-circle"
-                                  viewBox="0 0 16 16"
-                                >
-                                  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
-                                  <path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445" />
-                                </svg>
+                                <PlayIcon />
+                                // <svg
+                                //   xmlns="http://www.w3.org/2000/svg"
+                                //   width="20"
+                                //   height="20"
+                                //   fill="#fff"
+                                //   class="bi bi-play-circle"
+                                //   viewBox="0 0 16 16"
+                                // >
+                                //   <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+                                //   <path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445" />
+                                // </svg>
                               )}
                               {item.icon === "paper" && (
-                                <svg
-                                  xmlns="http://www.w3.org/2000/svg"
-                                  width="20"
-                                  height="20"
-                                  fill="#fff"
-                                  class="bi bi-paperclip"
-                                  viewBox="0 0 16 16"
-                                >
-                                  <path d="M4.5 3a2.5 2.5 0 0 1 5 0v9a1.5 1.5 0 0 1-3 0V5a.5.5 0 0 1 1 0v7a.5.5 0 0 0 1 0V3a1.5 1.5 0 1 0-3 0v9a2.5 2.5 0 0 0 5 0V5a.5.5 0 0 1 1 0v7a3.5 3.5 0 1 1-7 0z" />
-                                </svg>
+                                <PaperIcon />
+                                // <svg
+                                //   xmlns="http://www.w3.org/2000/svg"
+                                //   width="20"
+                                //   height="20"
+                                //   fill="#fff"
+                                //   class="bi bi-paperclip"
+                                //   viewBox="0 0 16 16"
+                                // >
+                                //   <path d="M4.5 3a2.5 2.5 0 0 1 5 0v9a1.5 1.5 0 0 1-3 0V5a.5.5 0 0 1 1 0v7a.5.5 0 0 0 1 0V3a1.5 1.5 0 1 0-3 0v9a2.5 2.5 0 0 0 5 0V5a.5.5 0 0 1 1 0v7a3.5 3.5 0 1 1-7 0z" />
+                                // </svg>
                               )}
                               {item.icon == "file" && (
-                                <svg
-                                  xmlns="http://www.w3.org/2000/svg"
-                                  width="20"
-                                  height="20"
-                                  fill="#fff"
-                                  class="bi bi-file-text"
-                                  viewBox="0 0 16 16"
-                                >
-                                  <path d="M5 4a.5.5 0 0 0 0 1h6a.5.5 0 0 0 0-1zm-.5 2.5A.5.5 0 0 1 5 6h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1-.5-.5M5 8a.5.5 0 0 0 0 1h6a.5.5 0 0 0 0-1zm0 2a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1z" />
-                                  <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2zm10-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1" />
-                                </svg>
+                                <FileIcon />
+                                // <svg
+                                //   xmlns="http://www.w3.org/2000/svg"
+                                //   width="20"
+                                //   height="20"
+                                //   fill="#fff"
+                                //   class="bi bi-file-text"
+                                //   viewBox="0 0 16 16"
+                                // >
+                                //   <path d="M5 4a.5.5 0 0 0 0 1h6a.5.5 0 0 0 0-1zm-.5 2.5A.5.5 0 0 1 5 6h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1-.5-.5M5 8a.5.5 0 0 0 0 1h6a.5.5 0 0 0 0-1zm0 2a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1z" />
+                                //   <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2zm10-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1" />
+                                // </svg>
                               )}
                             </div>
                             {item.context}
